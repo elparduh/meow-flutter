b6122606c3a4accd65b12d888ecf17c2ce4ea7ea
elparduh <ticantevicentejuan@outlook.com>
2024-09-16 18:15:45 -0600

    add CatRemoteDataSource

--- lib/feature/data/remote/cat_remote_datasource.dart
+++ lib/feature/data/remote/cat_remote_datasource.dart
@@ -1,3 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'package:meow_generator/core/network/http_request_handler.dart';
+
 class CatRemoteDataSource {
+
+  const CatRemoteDataSource(
+      this._client,
+      this._httpRequestHandler,
+      );
+
+  final http.Client _client;
+  final HttpRequestHandler _httpRequestHandler;
 
 }
