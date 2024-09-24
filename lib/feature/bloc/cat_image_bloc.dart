870f9cb09133505528785dc4ccf56577285bb36f
elparduh <ticantevicentejuan@outlook.com>
2024-09-24 13:27:55 -0600

    add CatImageState

--- lib/feature/bloc/cat_image_bloc.dart
+++ lib/feature/bloc/cat_image_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:equatable/equatable.dart';
+import 'package:meow_generator/core/state/ui_state.dart';
+import 'package:meow_generator/feature/ui/cat_image_ui_model.dart';
 
 part 'cat_image_event.dart';
 part 'cat_image_state.dart';
